b6a2a7817e8235dca2445ed7d3ac22c7d62e01f3
ProgRub <ruben2000jgr@gmail.com>
2019-07-05 15:40:34 +0100

    Print Comb n digits

---
+++ printcombn.go
@@ -0,0 +1,72 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintCombN(n int){
+z01.PrintRune(rune(39))make([]int,n)
+	indice:=0
+	cod:=48
+	for indice<n{
+		vet[indice]=cod
+		indice++
+		cod++
+	}
+	pos:=n-1
+	aux(n,vet, pos)
+	imprime(vet,n)
+z01.PrintRune(rune(39))
+}
+
+func aux(n int, v []int, pos int){
+	if v[0]==(58-n){
+		return;
+	}else{
+		imprime(v,n)
+		z01.PrintRune(',')
+		if v[pos]<57{
+			v[pos]++
+			aux(n,v, pos)
+		}else{
+			pos--
+			if pos>=0{
+				if pos>=0 && v[pos]==(57-(n-pos-1)){
+					pos--
+				}
+				if pos>=0 && v[pos]==(57-(n-pos-1)){
+					pos--
+				}
+				if pos>=0 && v[pos]==(57-(n-pos-1)){
+					pos--
+				}
+				if pos>=0 && v[pos]==(57-(n-pos-1)){
+					pos--
+				}
+				if pos>=0 && v[pos]==(57-(n-pos-1)){
+					pos--
+				}
+				if pos>=0 && v[pos]==(57-(n-pos-1)){
+					pos--
+				}
+				if pos>=0 && v[pos]==(57-(n-pos-1)){
+					pos--
+				}
+				v[pos]++
+			}	
+			for pos < n-1 && pos>=0{
+				v[pos+1]=v[pos]+1
+				pos++
+			}
+			aux(n,v, pos)
+		}
+	}
+}
+
+func imprime(v []int, tam int){
+	indice:=0
+	for indice<tam{
+		z01.PrintRune(rune(v[indice]))
+		indice++
+	}
+}
