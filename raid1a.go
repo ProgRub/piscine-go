2b1a872438992459c059b40634219c859006824d
ProgRub <ruben2000jgr@gmail.com>
2019-07-08 14:31:36 +0100

    raid1a

--- raid1a.go
+++ raid1a.go
@@ -3,28 +3,29 @@ package piscine
 import "fmt"
 
 func Raid1a(x, y int) {
-	edges := make([]rune, x)
-	middle := make([]rune, x)
-	for contaX := 0; contaX < x; contaX++ {
-		if contaX == 0 || contaX == x-1 {
-			edges[contaX] = 'o'
-		} else {
-			edges[contaX] = '-'
+	if x > 0 {
+		edges := make([]rune, x)
+		middle := make([]rune, x)
+		for contaX := 0; contaX < x; contaX++ {
+			if contaX == 0 || contaX == x-1 {
+				edges[contaX] = 'o'
+			} else {
+				edges[contaX] = '-'
+			}
+		}
+		for contaX := 0; contaX < x; contaX++ {
+			if contaX == 0 || contaX == x-1 {
+				middle[contaX] = '|'
+			} else {
+				middle[contaX] = ' '
+			}
+		}
+		for contaY := 0; contaY < y; contaY++ {
+			if contaY == 0 || contaY == y-1 {
+				fmt.Println(string(edges))
+			} else {
+				fmt.Println(string(middle))
+			}
 		}
 	}
-	for contaX := 0; contaX < x; contaX++ {
-		if contaX == 0 || contaX == x-1 {
-			middle[contaX] = '|'
-		} else {
-			middle[contaX] = ' '
-		}
-	}
-	for contaY := 0; contaY < y; contaY++ {
-		if contaY == 0 || contaY == y-1 {
-			fmt.Println(string(edges))
-		} else {
-			fmt.Println(string(middle))
-		}
-	}
-
 }
