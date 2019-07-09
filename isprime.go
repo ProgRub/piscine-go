1a00751c9b66723c136ab3aad92835dcf3fd4718
ProgRub <ruben2000jgr@gmail.com>
2019-07-09 10:12:01 +0100

    Is Prime

--- isprime.go
+++ isprime.go
@@ -2,16 +2,15 @@ package piscine
 
 func IsPrime(nb int) bool {
 	if nb > 1 {
-		multiplier := nb / 6
 		if nb > 3 {
 			if nb%2 == 0 {
 				return false
 			}
-			for 6*multiplier-1 <= nb || 6*multiplier+1 <= nb {
-				if 6*multiplier-1 == nb || 6*multiplier+1 == nb {
+
+			for i := 5; i*i < nb; i = i + 6 {
+				if nb%i == 0 || nb%(i+2) == 0 {
 					return true
 				}
-				multiplier++
 			}
 			return false
 		}
