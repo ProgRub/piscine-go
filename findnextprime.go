e0e4f8fcf2b793bc19373a840828095a7cba5d08
ProgRub <ruben2000jgr@gmail.com>
2019-07-09 10:17:47 +0100

    Find Next Prime

--- findnextprime.go
+++ findnextprime.go
@@ -11,22 +11,25 @@ const (
 
 func FindNextPrime(nb int) int {
 	if nb > 1 && nb < MaxInt {
-		i := 0
-		if nb%2 == 0 {
-			i = nb + 1
-		} else {
-			i = nb
-		}
-		for true {
-			if i < MaxInt {
-				if IsPrime(i) {
-					return i
-				}
-				i = i + 2
+		if nb > 3 {
+			i := 0
+			if nb%2 == 0 {
+				i = nb + 1
 			} else {
-				return 0
+				i = nb
+			}
+			for true {
+				if i < MaxInt {
+					if IsPrime(i) {
+						return i
+					}
+					i = i + 2
+				} else {
+					return 0
+				}
 			}
 		}
+		return nb
 	}
 	return 2
 }
