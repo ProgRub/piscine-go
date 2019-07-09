1e7f6ae85e9c91d485fe652d15ab7bcfb690e398
ProgRub <ruben2000jgr@gmail.com>
2019-07-09 10:14:13 +0100

    Is Prime

--- isprime.go
+++ isprime.go
@@ -6,13 +6,12 @@ func IsPrime(nb int) bool {
 			if nb%2 == 0 {
 				return false
 			}
-
 			for i := 5; i*i < nb; i = i + 6 {
 				if nb%i == 0 || nb%(i+2) == 0 {
-					return true
+					return false
 				}
 			}
-			return false
+			return true
 		}
 		return true
 	}
