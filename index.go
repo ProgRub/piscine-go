aaff41ef9f119192197796e43ea026cec5d5811c
ProgRub <ruben2000jgr@gmail.com>
2019-07-10 17:30:17 +0100

    Index

--- index.go
+++ index.go
@@ -6,7 +6,7 @@ func Index(s string, toFind string) int {
 	pos := 0
 	beginning := pos
 	for i := range auxString {
-		if auxToFind[pos] == auxString[i] {
+		if auxToFind[pos] == auxString[i] && pos < len(auxToFind) {
 			if pos == 0 {
 				beginning = i
 			}
