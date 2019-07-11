108c99d3956babc5af31cb7e7c5190e7bdccd698
ProgRub <ruben2000jgr@gmail.com>
2019-07-11 10:33:11 +0100

    Capitalize

--- capitalize.go
+++ capitalize.go
@@ -9,17 +9,19 @@ func Capitalize(s string) string {
 			if int(aux[i]) >= 65 && int(aux[i]) <= 90 {
 				aux[i] = aux[i] + 32
 			}
-			maiusculaAnterior = false
 		}
-		if espacoAnterior {
+		if espacoAnterior || i == 0 {
 			if int(aux[i]) >= 97 && int(aux[i]) <= 122 {
 				aux[i] = aux[i] - 32
+				maiusculaAnterior = true
+			} else if int(aux[i]) >= 48 && int(aux[i]) <= 57 {
 				maiusculaAnterior = true
 			}
 			espacoAnterior = false
 		}
 		if aux[i] == 32 || !((int(aux[i]) >= 97 && int(aux[i]) <= 122) || (int(aux[i]) >= 65 && int(aux[i]) <= 90) || (int(aux[i]) >= 48 && int(aux[i]) <= 57)) {
 			espacoAnterior = true
+			maiusculaAnterior = false
 		}
 	}
 	return string(aux)
