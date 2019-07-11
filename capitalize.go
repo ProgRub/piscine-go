50b4e6af669631e8b0a56192ab1fe073fc2df93d
ProgRub <ruben2000jgr@gmail.com>
2019-07-11 10:24:36 +0100

    Capitalize

---
+++ capitalize.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Capitalize(s string) string {
+	aux := []rune(s)
+	espacoAnterior := false
+	maiusculaAnterior := false
+	for i := range aux {
+		if maiusculaAnterior {
+			if int(aux[i]) >= 65 && int(aux[i]) <= 90 {
+				aux[i] = aux[i] + 32
+			}
+			maiusculaAnterior = false
+		}
+		if espacoAnterior {
+			if int(aux[i]) >= 97 && int(aux[i]) <= 122 {
+				aux[i] = aux[i] - 32
+				maiusculaAnterior = true
+			}
+			espacoAnterior = false
+		}
+		if aux[i] == 32 || !((int(aux[i]) >= 97 && int(aux[i]) <= 122) || (int(aux[i]) >= 65 && int(aux[i]) <= 90) || (int(aux[i]) >= 48 && int(aux[i]) <= 57)) {
+			espacoAnterior = true
+		}
+	}
+	return string(aux)
+}
