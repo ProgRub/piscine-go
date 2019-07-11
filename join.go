038c3862e2aa4c62d9d7fef45e69a76b4d063b64
ProgRub <ruben2000jgr@gmail.com>
2019-07-11 11:03:42 +0100

    Join

--- join.go
+++ join.go
@@ -3,7 +3,9 @@ package piscine
 func Join(strs []string, sep string) string {
 	var final string = strs[0]
 	for i := range strs {
-		if i != 0 && i != len(strs)-1 {
+		if i == 0 {
+			final = Concat(final, sep)
+		} else if i != len(strs)-1 {
 			final = Concat(final, strs[i])
 			final = Concat(final, sep)
 		} else if i == len(strs)-1 {
