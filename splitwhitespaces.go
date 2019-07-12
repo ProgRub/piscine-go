bcd850acf3d4c70b226027920964c4b084754a56
ProgRub <ruben2000jgr@gmail.com>
2019-07-12 16:20:13 +0100

    Split White Spaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,17 +1,7 @@
 package piscine
 
+import "strings"
+
 func SplitWhiteSpaces(str string) []string {
-	aux := []rune(str)
-	final := make([]string, 0)
-	palavra := make([]rune, 0)
-	for i := range aux {
-		if int(aux[i]) != int(' ') && int(aux[i]) != int('\t') && int(aux[i]) != int('\n') {
-			palavra = append(palavra, aux[i])
-		} else {
-			final = append(final, string(palavra))
-			palavra = make([]rune, 0)
-		}
-	}
-	final = append(final, string(palavra))
-	return final
+	return strings.Fields(str)
 }
