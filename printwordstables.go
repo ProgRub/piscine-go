45eda65eeaeb060f04daf97fef191ec090f17e30
ProgRub <ruben2000jgr@gmail.com>
2019-07-12 16:18:42 +0100

    Print Words Tables

---
+++ printwordstables.go
@@ -0,0 +1,12 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintWordsTables(table []string) {
+	toPrint := []rune(Join(table, "\n"))
+	for i := range toPrint {
+		z01.PrintRune(toPrint[i])
+	}
+}
