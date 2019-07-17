8e8920d507a38b241965378a8a715f376021d183
ProgRub <ruben2000jgr@gmail.com>
2019-07-17 10:54:15 +0100

    Doop

--- doop/doop.go
+++ doop/doop.go
@@ -9,16 +9,16 @@ import (
 func main() {
 	arguments := os.Args
 	if len(arguments) == 4 {
-		a, err := strconv.Atoi(arguments[1])
-		b, e := strconv.Atoi(arguments[3])
-		if err != nil {
-			fmt.Println("0")
-			return
-		}
-		if e != nil {
-			fmt.Println("0")
-			return
-		}
+		a, _ := strconv.Atoi(arguments[1])
+		b, _ := strconv.Atoi(arguments[3]) /*
+			if err != nil {
+				fmt.Println("0")
+				return
+			}
+			if e != nil {
+				fmt.Println("0")
+				return
+			} */
 		operador := []rune(arguments[2])
 		if (int(operador[0]) != 37 && int(operador[0]) != 42 && int(operador[0]) != 43 && int(operador[0]) != 45 && int(operador[0]) != 47) || len(operador) > 1 {
 			fmt.Println("0")
