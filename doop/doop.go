5423a23e05b59e80209306ca9d296acca7b29736
ProgRub <ruben2000jgr@gmail.com>
2019-07-17 10:14:43 +0100

    Doop

---
+++ doop/doop.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 4 {
+		a, err := strconv.Atoi(arguments[1])
+		b, e := strconv.Atoi(arguments[3])
+		if err != nil {
+			fmt.Println("0")
+			return
+		}
+		if e != nil {
+			fmt.Println("0")
+			return
+		}
+		operador := []rune(arguments[2])
+		if (int(operador[0]) != 37 && int(operador[0]) != 42 && int(operador[0]) != 43 && int(operador[0]) != 45 && int(operador[0]) != 47) || len(operador) > 1 {
+			fmt.Println("0")
+			return
+		}
+		if int(operador[0]) == 37 {
+			if b == 0 {
+				fmt.Println("No modulo by 0")
+			} else {
+				fmt.Println(a % b)
+			}
+		} else if int(operador[0]) == 42 {
+			fmt.Println(a * b)
+		} else if int(operador[0]) == 43 {
+			fmt.Println(a + b)
+		} else if int(operador[0]) == 45 {
+			fmt.Println(a - b)
+		} else if int(operador[0]) == 47 {
+			if b == 0 {
+				fmt.Println("No division by 0")
+			} else {
+				fmt.Println(a / b)
+			}
+		}
+	}
+}
