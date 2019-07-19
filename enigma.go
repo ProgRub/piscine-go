5c51c44d3fad68639318921d4798a1202e1227f9
ProgRub <ruben2000jgr@gmail.com>
2019-07-19 11:33:27 +0100

    Enigma

---
+++ enigma.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	auxC := *******c
+	auxB := *b
+	auxD := ****d
+	auxA := ***a
+	*******c = auxA
+	****d = auxC
+	*b = auxD
+	***a = auxB
+}
