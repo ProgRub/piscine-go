cd2c4c574647716cafeea58be8a6b50682c3846f
ProgRub <ruben2000jgr@gmail.com>
2019-07-19 14:07:40 +0100

    Collatz Countdown

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,14 +1,17 @@
 package piscine
 
 func CollatzCountdown(start int) int {
-	conta := 1
-	for start != 1 {
-		if start%2 == 0 {
-			start /= 2
-		} else {
-			start = start*3 + 1
+	if start >= 1 {
+		conta := 1
+		for start != 1 {
+			if start%2 == 0 {
+				start /= 2
+			} else {
+				start = start*3 + 1
+			}
+			conta++
 		}
-		conta++
+		return conta
 	}
-	return conta
+	return 0
 }
