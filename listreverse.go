c3ac83addb226d95be645010361053daa68b153d
ProgRub <ruben2000jgr@gmail.com>
2019-07-19 16:29:08 +0100

    List Reverse

--- listreverse.go
+++ listreverse.go
@@ -3,17 +3,15 @@ package piscine
 func ListReverse(l *List) {
 	if l.Head != nil {
 		if ListSize(l) > 1 {
-			dados := make([]interface{}, ListSize(l))
+			dados := make([]interface{}, 0)
 			itera := l.Head
 			for itera != nil {
 				dados = append(dados, itera.Data)
 				itera = itera.Next
 			}
 			pos := 0
-			final := &List{}
-			final.Head = nil
-			final.Tail = nil
-			for pos < len(dados)/2 {
+			final := &List{Head: nil, Tail: nil}
+			for pos < len(dados) {
 				ListPushFront(final, dados[pos])
 				pos++
 			}
