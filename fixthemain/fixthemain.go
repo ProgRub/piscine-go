a0fcb593bc22a75d34322b421929620be01b3cc4
ProgRub <ruben2000jgr@gmail.com>
2019-07-19 11:34:13 +0100

    Fix The Main

---
+++ fixthemain/fixthemain.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+type Door struct {
+	state string
+}
+
+func PrintStr(str string) {
+	arrayRune := []rune(str)
+	for _, s := range arrayRune {
+		z01.PrintRune(s)
+	}
+}
+
+func CloseDoor(ptrDoor *Door) bool {
+	PrintStr("Door Closing...")
+	ptrDoor.state = "CLOSE"
+	return true
+}
+
+func OpenDoor(ptrDoor *Door) bool {
+	PrintStr("Door Opening...")
+	ptrDoor.state = "OPEN"
+	return false
+}
+
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("is the Door opened ?")
+	return ptrDoor.state == "OPEN"
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	return ptrDoor.state == "CLOSE"
+}
+
+func main() {
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+	if door.state == "OPEN" {
+		CloseDoor(door)
+	}
+}
