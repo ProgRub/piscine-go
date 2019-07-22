fe0287fde6d6af5053b7bffdd066556a4a8ea7c1
ProgRub <ruben2000jgr@gmail.com>
2019-07-22 14:18:02 +0100

    List Reverse

--- listreverse.go
+++ listreverse.go
@@ -1,6 +1,33 @@
 package piscine
 
 func ListReverse(l *List) {
+	if l.Head != nil {
+		if ListSize(l) > 2 {
+			anterior := l.Head
+			meio := l.Head.Next
+			proximo := l.Head.Next.Next
+			l.Head.Next = nil
+			l.Head = l.Tail
+			l.Tail = anterior
+			for proximo != nil {
+				meio.Next = anterior
+				anterior = meio
+				meio = proximo
+				proximo = proximo.Next
+			}
+			meio.Next = anterior
+
+		} else if ListSize(l) == 2 {
+			l.Tail.Next = l.Head
+			l.Head.Next = nil
+			aux := l.Head
+			l.Head = l.Tail
+			l.Tail = aux
+		}
+	}
+}
+
+/* func ListReverse(l *List) {
 	anterior := &NodeL{}
 	anterior = nil
 	meio := l.Head
@@ -15,3 +42,4 @@ func ListReverse(l *List) {
 		meio = proximo
 	}
 }
+*/
