20a0c99fbdf4cb3f6849411eea0a29716d93913c
ProgRub <ruben2000jgr@gmail.com>
2019-07-23 17:10:37 +0100

    BST Transplant

--- btreetransplant.go
+++ btreetransplant.go
@@ -8,6 +8,31 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 		} else if node.Data > copia.Data {
 			copia = copia.Right
 		} else {
+			if copia != root {
+				anterior := copia.Parent
+				if anterior.Left.Data == copia.Data {
+					anterior.Left = rplc
+				} else {
+					anterior.Right = rplc
+				}
+			} else {
+				root = rplc
+			}
+			copia = nil
+			return root
+		}
+	}
+	return root
+}
+
+/* func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
+	copia := root
+	for copia != nil {
+		if node.Data < copia.Data {
+			copia = copia.Left
+		} else if node.Data > copia.Data {
+			copia = copia.Right
+		} else {
 			rplc.Parent = copia.Parent
 			rplc.Left = copia.Left
 			rplc.Right = copia.Right
@@ -27,3 +52,4 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 	}
 	return root
 }
+*/
