c99edc588f9b6b1495af8144f31719dfb75a9a61
ProgRub <ruben2000jgr@gmail.com>
2019-07-23 16:46:01 +0100

    BST Insert Data

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -9,19 +9,17 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		novo := &TreeNode{Data: data, Left: nil, Right: nil, Parent: nil}
 		root = novo
-	} else if data <= root.Data {
+	} else if data < root.Data {
 		if root.Left == nil {
-			novo := &TreeNode{Data: data, Left: nil, Right: nil, Parent: nil}
+			novo := &TreeNode{Data: data, Left: nil, Right: nil, Parent: root}
 			root.Left = novo
-			novo.Parent = root
 		} else {
 			root = BTreeInsertData(root.Left, data)
 		}
 	} else {
 		if root.Right == nil {
-			novo := &TreeNode{Data: data, Left: nil, Right: nil, Parent: nil}
+			novo := &TreeNode{Data: data, Left: nil, Right: nil, Parent: root}
 			root.Right = novo
-			novo.Parent = root
 		} else {
 			root = BTreeInsertData(root.Right, data)
 		}
